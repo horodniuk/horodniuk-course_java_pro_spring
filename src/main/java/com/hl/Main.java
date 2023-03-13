2b7630af2326e804c52d706cbf820f1db50816b7
horodniuk <horodniuk.maksym55@gmail.com>
2023-03-13 23:21:31 +0200

    add Main class

---
+++ src/main/java/com/hl/Main.java
@@ -0,0 +1,12 @@
+package com.hl;
+
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class Main {
+    public static void main(String[] args) {
+        log.info("Start");
+        System.out.println("Hello");
+    }
+}
