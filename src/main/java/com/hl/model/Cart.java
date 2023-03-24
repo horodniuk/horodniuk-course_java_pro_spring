1a40c27dc5ad12cd7def3c20175c4008d7cbd9f0
horodniuk <horodniuk.maksym55@gmail.com>
2023-03-24 03:28:54 +0200

    add Cart

---
+++ src/main/java/com/hl/model/Cart.java
@@ -0,0 +1,18 @@
+package com.hl.model;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Cart {
+    private Long id;
+    private List<Product> products;
+}
